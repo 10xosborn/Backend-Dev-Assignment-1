fa23e34c64c6b4ff47459bed379ebf5522cf50b6
Osborn Tulasi <osborntulasi@gmail.com>
2025-10-08 09:31:44 +0000

    Add intro.js file

---
+++ intro.js
@@ -0,0 +1,7 @@
+const my_name = 'Osborn Tulasi';
+
+const age = 21;
+
+const favourite_language = 'Python';
+
+console.log(`My name is ${my_name}, I am ${age} years old and my favourite programming language is ${favourite_language}.`);
